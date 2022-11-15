e3733f40eedd7c385c3f75668b7846714adcba8e
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-11-15 15:28:12 +0500

    theme update

---
+++ lib/screen/main_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class MianScreen extends StatelessWidget {
+  const MianScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('MianScreen')),
+    );
+  }
+}
