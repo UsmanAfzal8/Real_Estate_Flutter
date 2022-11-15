d9d87a9f6476b1389e393fc8280b25b7a0267259
Usman Afzal <56322052+UsmanAfzal8@users.noreply.github.com>
2022-11-16 02:47:04 +0500

    main screen update

--- lib/screen/main_screen.dart
+++ lib/screen/main_screen.dart
@@ -8,81 +8,83 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Column(
-          children: [
-            Container(
-              height: 250,
-              width: double.infinity,
-              color: Theme.of(context).primaryColor,
-              child: Column(
-                children: [
-                  const SizedBox(height: 20),
-                  Row(
-                    children: const [
-                      SizedBox(
-                        width: 10,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 250,
+                width: double.infinity,
+                color: Theme.of(context).primaryColor,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    Row(
+                      children: const [
+                        SizedBox(
+                          width: 10,
+                        ),
+                        CircleAvatar(
+                          radius: 20,
+                          backgroundColor: Colors.white,
+                          child: Icon(
+                            Icons.arrow_back_ios_sharp,
+                            size: 28,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.symmetric(
+                          horizontal: 26, vertical: 20),
+                      height: 60,
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(16),
+                        color: const Color.fromARGB(255, 44, 76, 89),
                       ),
-                      CircleAvatar(
-                        radius: 20,
-                        backgroundColor: Colors.white,
-                        child: Icon(
-                          Icons.arrow_back_ios_sharp,
-                          size: 28,
-                          color: Colors.black,
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 16),
+                        child: Row(
+                          children: const [
+                            Icon(
+                              Icons.search,
+                              color: Colors.grey,
+                            ),
+                            SizedBox(width: 20),
+                            Text(
+                              'Search for best result',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                        horizontal: 26, vertical: 20),
-                    height: 60,
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: const Color.fromARGB(255, 44, 76, 89),
                     ),
-                    child: Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 16),
-                      child: Row(
-                        children: const [
-                          Icon(
-                            Icons.search,
-                            color: Colors.grey,
-                          ),
-                          SizedBox(width: 20),
-                          Text(
-                            'Search for best result',
-                            style: TextStyle(color: Colors.grey),
-                          ),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  types('House', Icons.house),
-                  types('Office', Icons.other_houses_sharp),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    types('House', Icons.house),
+                    types('Office', Icons.other_houses_sharp),
+                  ],
+                ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  types('Shop', Icons.shop),
-                  types('Plot', Icons.houseboat_sharp),
-                ],
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    types('Shop', Icons.shop),
+                    types('Plot', Icons.houseboat_sharp),
+                  ],
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
